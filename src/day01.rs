e29c1abe360c631b6e9d191be4cfe5ad1e1ba751
Rafaël L. Bekkema <rafael@rafaelbekkema.nl>
2022-12-08 12:14:41 +0100

    Refactor some more

--- src/day01.rs
+++ src/day01.rs
@@ -8,10 +8,10 @@ pub fn b() -> u64 {
     _b(load_lines(1))
 }
 
-fn parse_lines(lines: &[String]) -> Vec<u64> {
+fn parse_lines(lines: &Vec<impl AsRef<str>>) -> Vec<u64> {
     let mut counts = Vec::from([0u64]);
 
-    for line in lines.iter() {
+    for line in lines.iter().map(|l| l.as_ref()) {
         if line.is_empty() {
             counts.push(0);
             continue;
@@ -22,11 +22,11 @@ fn parse_lines(lines: &[String]) -> Vec<u64> {
     counts
 }
 
-fn _a(lines: Vec<String>) -> u64 {
+fn _a(lines: Vec<impl AsRef<str>>) -> u64 {
     parse_lines(&lines).into_iter().max().unwrap()
 }
 
-fn _b(lines: Vec<String>) -> u64 {
+fn _b(lines: Vec<impl AsRef<str>>) -> u64 {
     let mut counts = parse_lines(&lines);
 
     counts.sort();
@@ -42,20 +42,20 @@ mod tests {
         assert_eq!(
             24000,
             _a(vec![
-                "1000".to_string(),
-                "2000".to_string(),
-                "3000".to_string(),
-                "".to_string(),
-                "4000".to_string(),
-                "".to_string(),
-                "5000".to_string(),
-                "6000".to_string(),
-                "".to_string(),
-                "7000".to_string(),
-                "8000".to_string(),
-                "9000".to_string(),
-                "".to_string(),
-                "10000".to_string(),
+                "1000",
+                "2000",
+                "3000",
+                "",
+                "4000",
+                "",
+                "5000",
+                "6000",
+                "",
+                "7000",
+                "8000",
+                "9000",
+                "",
+                "10000",
             ])
         );
     }
@@ -70,20 +70,20 @@ mod tests {
         assert_eq!(
             45000,
             _b(vec![
-                "1000".to_string(),
-                "2000".to_string(),
-                "3000".to_string(),
-                "".to_string(),
-                "4000".to_string(),
-                "".to_string(),
-                "5000".to_string(),
-                "6000".to_string(),
-                "".to_string(),
-                "7000".to_string(),
-                "8000".to_string(),
-                "9000".to_string(),
-                "".to_string(),
-                "10000".to_string(),
+                "1000",
+                "2000",
+                "3000",
+                "",
+                "4000",
+                "",
+                "5000",
+                "6000",
+                "",
+                "7000",
+                "8000",
+                "9000",
+                "",
+                "10000",
             ])
         );
     }
